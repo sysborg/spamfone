5db78d75854576d88e783a4e3733114c04abaeb3
Anderson ARruda <andmarruda@gmail.com>
2022-05-09 18:57:07 -0300

    Factory with Faker for ReportedSpam

--- database/factories/ReportedSpamFactory.php
+++ database/factories/ReportedSpamFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\ReportedSpam;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ReportedSpam>
@@ -17,7 +18,15 @@ class ReportedSpamFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'contact_type_id'   => $this->faker->numberBetween(1, 3), 
+            'spam_type_id'      => $this->faker->numberBetween(1, 11), 
+            'ip'                => $this->faker->ipv4(), 
+            'caller_name'       => $this->faker->name(), 
+            'reported_name'     => $this->faker->name(), 
+            'description'       => $this->faker->realText(), 
+            'grade'             => $this->faker->numberBetween(0, 9), 
+            'cell'              => $this->faker->e164PhoneNumber(), 
+            'email'             => $this->faker->email()
         ];
     }
 }
