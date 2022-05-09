c7dd0847736428cfab1087b29e214c2445f300d0
Anderson ARruda <andmarruda@gmail.com>
2022-05-09 18:39:16 -0300

    Adding ReportedSpamFactory and fillable columns to RepotedSpam model

--- app/Models/ReportedSpam.php
+++ app/Models/ReportedSpam.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ReportedSpam extends Model
 {
     use HasFactory;
+    protected $fillable = ['contact_type_id', 'spam_type_id', 'ip', 'caller_name', 'reported_name', 'description', 'grade', 'cell', 'email'];
 }
