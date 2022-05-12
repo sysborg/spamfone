59f32cc139d8d692999972ad468177c9656be71a
Anderson ARruda <andmarruda@gmail.com>
2022-05-12 18:54:03 -0300

    Template corrections and improvements

--- resources/views/template/header.blade.php
+++ resources/views/template/header.blade.php
@@ -2,13 +2,9 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-12 head-bg">
-                        <div class="content d-flex justify-content-center">
-                            <i class="fa-solid fa-mobile"></i>
-                            <div style="margin: 0 32px;">
-                                <h1>{{$title}}</h1>
-                                <p class="slogan">{{$slogan}}</p>
-                            </div>
-                            <i class="fa-solid fa-envelope"></i>
+                        <div class="content d-flex flex-column justify-content-center">
+                            <img src="{{asset('images/logo.png')}}" alt="{{$title}}" class="img-fluid logo">
+                            <p class="slogan">{{$slogan}}</p>
                         </div>
                     </div>
                 </div>
