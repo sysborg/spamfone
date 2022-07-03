2ec0773b423ddf1d94733ce3e4f7f6b78ad871d7
Anderson ARruda <andmarruda@gmail.com>
2022-07-03 19:54:56 -0300

    Adding belongsto spam types and reported data into ReportedSpam

--- app/Models/ReportedSpam.php
+++ app/Models/ReportedSpam.php
@@ -9,4 +9,28 @@ class ReportedSpam extends Model
 {
     use HasFactory;
     protected $fillable = ['spam_type_id', 'reported_data_id', 'ip', 'reported_name', 'whistleblower_name', 'comment', 'grade'];
+
+    /**
+     * Get spam types of reported spam.
+     * @version     1.0.0
+     * @author      Anderson Arruda < andmarruda@gmail.com >
+     * @param       
+     * @return      Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function spamTypes() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(SpamType::class);
+    }
+
+    /**
+     * Get reported date of reported spam.
+     * @version     1.0.0
+     * @author      Anderson Arruda < andmarruda@gmail.com >
+     * @param       
+     * @return      Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function reportedSpam() : \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(ReportedSpam::class);
+    }
 }
