3d3dcc9931875c050513797a6d80743cce5316da
Anderson ARruda <andmarruda@gmail.com>
2022-07-03 15:58:57 -0300

    adding fillable at model ReportedSpam

--- app/Models/ReportedSpam.php
+++ app/Models/ReportedSpam.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ReportedSpam extends Model
 {
     use HasFactory;
+    protected $fillable = ['spam_type_id', 'reported_data_id', 'ip', 'reported_name', 'whistleblower_name', 'comment', 'grade'];
 }
